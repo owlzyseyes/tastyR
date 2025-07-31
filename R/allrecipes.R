5a192172c2f51a1bafd7b8ec328110ded6515a85
Brian <monsieursapien@gmail.com>
2025-07-31 16:49:00 +0300

    Updated allrecipes dataset: Fixed NAs in ratings associated columns.

--- R/allrecipes.R
+++ R/allrecipes.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset of recipes scraped from Allrecipes.com, containing ingredients, cooking times, nutrition facts, ratings, and review counts. The data has been parsed and cleaned to ensure analysis-readiness.
 #'
-#' @format A data frame with 14420 rows and 16 variables:
+#' @format A data frame with 14426 rows and 16 variables:
 #' \describe{
 #'   \item{name}{Name of the recipe}
 #'   \item{url}{Link to the recipe}
@@ -14,8 +14,8 @@
 #'   \item{carbs}{carbs per serving}
 #'   \item{protein}{Proteins per serving}
 #'   \item{avg_rating}{Average rating out of 5 stars}
-#'   \item{n_ratings}{Number of ratings received}
-#'   \item{n_reviews}{Number of written reviews}
+#'   \item{total_ratings}{Number of ratings received}
+#'   \item{reviews}{Number of written reviews}
 #'   \item{prep_time}{Preparation time in minutes}
 #'   \item{cook_time}{Cooking time in minutes}
 #'   \item{total_time}{Prep + cook time in minutes.Note that this value may not always match the actual total effort required, as other time-related fields (such as refrigeration, marination, fry time, or additional wait periods) have been excluded due to inconsistent availability across recipes}
